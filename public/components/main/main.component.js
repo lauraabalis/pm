5b441c0c10e4a948640af0244c5dac31a3556540
Laura Balis <lauraab1@cox.net>
2017-06-08 16:48:26 -0700

    Fourth commit

--- public/components/main/main.component.js
+++ public/components/main/main.component.js
@@ -7,16 +7,52 @@
           controller: function () {
           // controller: function (taskService) {
             var ctrl = this;
-            ctrl.tasks = [];
-            ctrl.showTask = showTask;
+
+            ctrl.foodDrink = [];
+            ctrl.addFoodDrink = addFoodDrink;
+
+            ctrl.activities = [];
+            ctrl.addActivity = addActivity;
+
+            ctrl.decor = [];
+            ctrl.addDecor = addDecor;
+
+            ctrl.partyFavors = [];
+            ctrl.addPartyFavor = addPartyFavor;
             //ctrl.tasks = taskService.tasks;
 
-            function showTask() {
-              ctrl.tasks.push({
-                task: ctrl.newTask.task
+            ctrl.activityCheckbox = false;
+
+            ctrl.activityColor = "red";
+
+            function addFoodDrink() {
+              ctrl.foodDrink.push({
+                task: ctrl.newFoodDrink
               });
-              ctrl.newTask.task = null;
+              ctrl.newFoodDrink = null;
             }
+
+            function addActivity() {
+              ctrl.activities.push({
+                task: ctrl.newActivity
+              });
+              ctrl.newActivity = null;
+            }
+
+            function addDecor() {
+              ctrl.decor.push({
+                task: ctrl.newDecor
+              });
+              ctrl.newDecor = null;
+            }
+
+            function addPartyFavor() {
+              ctrl.partyFavors.push({
+                task: ctrl.newPartyFavor
+              });
+              ctrl.newPartyFavor = null;
+            }
+
           }
         });
 
